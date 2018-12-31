901abfbd3f8ad4a39c03f0d19de1ec13d645bb93
Haaris Mian <mail@haarismian.com>
2018-12-31 10:45:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Spreadsheet';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
+
+import Spreadsheet from './components/Spreadsheet';
+import TextEdit from './components/TextEdit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.spreadsheet = this.spreadsheet.bind(this);
+    this.textEdit = this.textEdit.bind(this);
+  }
+
+  spreadsheet() {
+    ReactDOM.render(<Spreadsheet />, document.getElementById('content'));
+  }
+
+  textEdit() {
+    ReactDOM.render(<TextEdit />, document.getElementById('content'));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header>
+            <FlatButton label="spreadsheet" onClick={this.spreadsheet} />
+            <FlatButton label="text edit" onClick={this.textEdit} />
+          </header>
+          <div id="content" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
